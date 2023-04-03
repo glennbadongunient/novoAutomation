fd6788907cbebc6bdab4d58b1640fb1d304ab9e9
Glenn Badong <glenn.badong@unient.biz>
2023-04-03 14:16:02 +0800

    fixed checking of meta keywords and meta description

--- tests/unientWebsite.spec.js
+++ tests/unientWebsite.spec.js
@@ -1,8 +1,9 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const fs = require('fs');
 const url = 'https://stage-unientwww.euwest01.umbraco.io/blog/covid-and-offshore-outsourcing/';
 
-test('getting page source', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
+    // This will fetch the whole page source
     
     // Open url and login
     await page.goto(url);
@@ -21,6 +22,53 @@ test('getting page source', async ({ page }) => {
     // Save page source to file
     fs.writeFileSync('pageSource.txt', pageSource);
 
+});
+
+test.afterEach(async ({ page }) =>{
+
     // close page
     await page.close();
+
+});
+
+test('validate meta keywords', async ({ page }) => {
+
+    const {evaluate} = require('@playwright/test');
+
+    // Validating if meta keywords are the expected values
+    const metaContent = await page.evaluate((name) => {
+        const meta = document.querySelector(`meta[name="keywords"]`);
+        const metCon = meta ? meta.getAttribute('content') : null;
+        return metCon;
+      }, 'description');
+
+    const expectedContent = "Offshore Outsourcing, Outsourcing Industry, Offshore outsourcing industry, remote work, remote working, digital transformation, outsourcing business, outsourcing talent";
+    // const message = metaContent === expectedContent
+    //     ? 'Meta tag keywords matches expected value.'
+    //     : 'Meta tag keywords does not match expected value.';
+    // console.log(message);
+    expect(metaContent).toEqual(expectedContent);
+    fs.writeFileSync('keywords.txt', metaContent);
+
+});
+    
+test('validate meta description', async ({ page }) => {
+
+    const {evaluate} = require('@playwright/test');
+
+    // Validating if meta description is the expected value
+    const metaContent2 = await page.evaluate((name) => {
+        const meta = document.querySelector(`meta[name="description"]`);
+        const metCon = meta ? meta.getAttribute('content') : null;
+        return metCon;
+      }, 'description');
+
+    const expectedContent2 = "COVID-19 affected numerous industries. Read this article to learn about the ways the pandemic impacted the offshore outsourcing industry.";
+    // const message2 = metaContent2 === expectedContent2
+    //     ? 'Meta tag description matches expected value.'
+    //     : 'Meta tag description does not match expected value.';
+    // console.log(message2);
+    expect(metaContent2).toEqual(expectedContent2);
+    fs.writeFileSync('description.txt', metaContent2);
+
 });
