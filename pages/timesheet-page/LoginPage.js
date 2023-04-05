6696b8117ebca57dd5bd5fecf21225a3bab0335f
Glenn Badong <glenn.badong@unient.biz>
2023-04-05 16:38:12 +0800

    making clear time entries

--- pages/timesheet-page/LoginPage.js
+++ pages/timesheet-page/LoginPage.js
@@ -7,29 +7,35 @@ export class LoginPage {
 
     async goto(){
         await this.page.goto("https://unienttimesheetfe.azurewebsites.net/timesheet");
-        await this.page.waitForLoadState('networkidle');
+        await this.page.locator("#i0116");
     }
 
     async login(username, password) {
-        const usernameField = await this.page.locator("#i0116");
+        const usernameField = this.page.locator("#i0116");
         await usernameField.fill(username);
         
-        const nextButton = await this.page.locator("//input[contains(@type, 'submit') and @id='idSIButton9']");
+        const nextButton = this.page.locator("//input[contains(@type, 'submit') and @id='idSIButton9']");
         await nextButton.click();
         // await this.page.waitForLoadState('networkidle');
         
-        const passwordField = await this.page.locator("#i0118");
+        const passwordField = this.page.locator("#i0118");
         await passwordField.fill(password);
 
-        const signInButton = await this.page.locator("//input[@id='idSIButton9' and @type='submit']");
+        const signInButton = this.page.locator("//input[@id='idSIButton9' and @type='submit']");
         await signInButton.click();
         // await this.page.waitForLoadState('networkidle');
 
-        const noButton = await this.page.locator('#idBtn_Back');
+        const noButton = this.page.locator('#idBtn_Back');
         await noButton.click();
+
+        await this.page.waitForSelector("//div[contains(@class,'md:pl-2 text-center text-xl font-semibold') and text()=' Timesheet ']");
     }
 
     async pause() {
         await this.page.pause();
+    }
+
+    async closePage(){
+        await this.page.close();
     }
 }
--- tests/timesheetPOM.spec.js
+++ tests/timesheetPOM.spec.js
@@ -5,28 +5,38 @@ import { TimesheetHomepage } from "../pages/timesheet-page/TimesheetHomepage";
 
 test.use({
     // screenshot: 'only-on-failure',
-    // launchOptions:{slowMo: 1000},
+    // launchOptions:{slowMo: 2000},
     // viewport: {width:1920,height:1080},
     headless: false
 });
 
-// POM TRY
+// PAGE OBJECT MODEL TRY
 test('negative time', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const timesheetHomepage = new TimesheetHomepage(page);
     const newTimeEntry = new NewTimeEntry(page);
 
+    // LOGIN
     await loginPage.goto();
     await loginPage.login("auto-test@unient.biz", "c+7lwR-P*FiGaQu!R1pa");
-    await timesheetHomepage.deleteAllExistingTimeEntry();
+
+    // DELETE EXISTING TIME ENTRIES
+    // await timesheetHomepage.deleteAllExistingTimeEntry();
+
+    // FILL OUT FORM
     await timesheetHomepage.clickNewTimeEntry();
     await newTimeEntry.addDate();
     await newTimeEntry.addTime('09:00', '08:00');
     await newTimeEntry.addTask();
     await newTimeEntry.addNote();
     await newTimeEntry.clickAddTimeEntryButton();
+
+    // VALIDATE ERROR
     await newTimeEntry.validateEndOverStartTimeErrorMessage();
     await newTimeEntry.validateEndOverStartTimeErrorMessageColor();
+
+    // CLOSE PAGE
+    await loginPage.closePage();
 });
 
 test('add time entry', async ({ page }) => {
@@ -34,13 +44,40 @@ test('add time entry', async ({ page }) => {
     const timesheetHomepage = new TimesheetHomepage(page);
     const newTimeEntry = new NewTimeEntry(page);
 
+    // LOGIN
     await loginPage.goto();
     await loginPage.login("auto-test@unient.biz", "c+7lwR-P*FiGaQu!R1pa");
-    await timesheetHomepage.deleteAllExistingTimeEntry();
+
+    // DELETE EXISTING TIME ENTRIES
+    // await timesheetHomepage.deleteAllExistingTimeEntry();
+
+    // FILL OUT FORM
     await timesheetHomepage.clickNewTimeEntry();
     await newTimeEntry.addDate();
     await newTimeEntry.addTime('08:00', '09:00');
     await newTimeEntry.addTask();
     await newTimeEntry.addNote();
     await newTimeEntry.clickAddTimeEntryButton();
+
+    // VALIDATE TIME ENTRY CREATION IS SUCCESSFUL
+    // await page.pause();
+
+    // CLOSE PAGE
+    await loginPage.closePage();
 });
+
+// test('clear time entries', async ({ page }) => {
+//     const loginPage = new LoginPage(page);
+//     const timesheetHomepage = new TimesheetHomepage(page);
+//     // const newTimeEntry = new NewTimeEntry(page);
+
+//     // LOGIN
+//     await loginPage.goto();
+//     await loginPage.login("auto-test@unient.biz", "c+7lwR-P*FiGaQu!R1pa");
+
+//     // DELETE EXISTING TIME ENTRIES
+//     await timesheetHomepage.deleteAllExistingTimeEntry();
+
+//     // CLOSE PAGE
+//     await loginPage.closePage();
+// });
